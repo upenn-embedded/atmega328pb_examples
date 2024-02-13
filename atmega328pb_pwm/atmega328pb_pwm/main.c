53b37aee4d1e5d216f69b21244fb2246a49ed3f6
Nick McGill-Gardner <nmcgill@seas.upenn.edu>
2024-02-13 00:14:33 -0500

    added pwm via ctc

--- atmega328pb_pwm/atmega328pb_pwm/main.c
+++ atmega328pb_pwm/atmega328pb_pwm/main.c
@@ -3,16 +3,51 @@
  *
  * Created: 2/13/2024 12:01:21 AM
  * Author : nmcgill
- */ 
+ */
 
+#include <avr/interrupt.h>
 #include <avr/io.h>
 
+int high_time = 10907;
+int low_time = 25451;
+volatile int high_low = 1;   // start high
 
-int main(void)
-{
-    /* Replace with your application code */
-    while (1) 
-    {
+void Initialize() {
+
+    cli();   // Disable global interrupts
+
+    DDRB |= (1 << DDB1);      // Set PB1 to be an output pin
+    PORTB |= (1 << PORTB1);   // Start with output on
+
+    // Timer1, prescale of 1
+    TCCR1B |= (1 << CS10);
+
+    // Timer1, CTC mode
+    TCCR1B |= (1 << WGM12);
+
+    // Toggle on compare match
+    TCCR1A |= (1 << COM1A0);
+    OCR1A = 20;
+
+    TIMSK1 |= (1 << OCIE1A);   // Enable Interrupt
+
+    TIFR1 |= (1 << OCF1A);   // Clear interrupt flag
+
+    sei();   // Enable global interrupts
+}
+
+ISR(TIMER1_COMPA_vect) {
+    if (high_low) {
+        OCR1A = low_time;
+        high_low = 0;
+    } else {
+        OCR1A = high_time;
+        high_low = 1;
     }
 }
 
+int main(void) {
+    Initialize();
+    while (1)
+        ;
+}
