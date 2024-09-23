7cf747dd9513bed2a354269950364f406f1180be
Nick McGill-Gardner <nmcgill@upenn.edu>
2024-09-22 20:27:28 -0400

    updated to match lecture example

--- atmega328pb_interrupts/atmega328pb_interrupts/main.c
+++ atmega328pb_interrupts/atmega328pb_interrupts/main.c
@@ -6,11 +6,11 @@ void Initialize()
     DDRB |= (1 << DDB5);	 // Set PB5 as an output
     PORTB |= (1 << PORTB5);   // Turn off the User LED by default
 	
-    DDRB &= ~(1 << DDB7);	 // Set PB7 as input
-    //PORTB |= (1 << PORTB7);  // Enable pull-up resistor on PB7
+    DDRD &= ~(1 << DDD5);	 // Set PD5 as input
+    PORTD |= (1 << PORTD7);  // Enable pull-up resistor on PD5
 
-    PCICR |= (1 << PCIE0);	 // Enable Pin Change Interrupt for PCINT0-7
-    PCMSK0 |= (1 << PCINT7); // Enable Pin Change Interrupt for PB7
+    PCICR |= (1 << PCIE2);	 // Enable Pin Change Interrupt for PCINT16-23
+    PCMSK2 |= (1 << PCINT21); // Enable Pin Change Interrupt for PD5
     sei();					 // Enable global interrupts
 }
 
