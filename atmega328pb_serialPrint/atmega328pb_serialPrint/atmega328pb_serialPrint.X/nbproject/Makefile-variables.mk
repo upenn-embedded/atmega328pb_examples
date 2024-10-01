2f593ddc77b4577bf932f4187620b405cb7978fe
Nick McGill-Gardner <nmcgill@upenn.edu>
2024-10-01 13:49:44 -0400

    updated serial print example to work as an mplab project

---
+++ atmega328pb_serialPrint/atmega328pb_serialPrint/atmega328pb_serialPrint.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,14 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# Release configuration
+CND_ARTIFACT_DIR_Release=dist/Release/production
+CND_ARTIFACT_NAME_Release=atmega328pb_serialPrint.X.production.hex
+CND_ARTIFACT_PATH_Release=dist/Release/production/atmega328pb_serialPrint.X.production.hex
+# Debug configuration
+CND_ARTIFACT_DIR_Debug=dist/Debug/production
+CND_ARTIFACT_NAME_Debug=atmega328pb_serialPrint.X.production.hex
+CND_ARTIFACT_PATH_Debug=dist/Debug/production/atmega328pb_serialPrint.X.production.hex
