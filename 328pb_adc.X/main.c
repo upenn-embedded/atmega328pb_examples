57f1f2aedcf42ce89d77b869fd5e42faae6a4cb0
Nick McGill-Gardner <nmcgill@upenn.edu>
2025-02-16 22:34:59 -0500

    moving adc to new project

--- 328pb_adc.X/main.c
+++ 328pb_adc.X/main.c
@@ -1,27 +1,20 @@
-/*
- * atmega328pb_adc.c
- *
- * Created: 2/19/2024 8:23:03 PM
- * Author : nmcgill
- */
-
-#include "../../common_libraries/uart.h"
-#include <avr/delay.h>
-#include <avr/interrupt.h>
-#include <avr/io.h>
-#include <stdlib.h>
-
-#define F_CPU               16000000UL
-#define UART_BAUD_RATE      74880
-#define UART_BAUD_PRESCALER (((F_CPU / (UART_BAUD_RATE * 16UL))) - 1)
-#define __PRINT_NEW_LINE__  UART_putstring("\r\n");
-
 // Choose ONE example to run (and comment out all others)
-// #define ADC_READ_EXAMPLE
+//#define ADC_READ_EXAMPLE
 // #define TEMPERATURE_READ_EXAMPLE //Still needs a little work!
 #define SIGNAL_FREQ_MEASURE_EXAMPLE
 
 #ifdef ADC_READ_EXAMPLE
+// Description:
+#define F_CPU               16000000UL
+
+#include <xc.h>
+#include "../common_libraries/uart.h"
+#include <util/delay.h>
+#include <stdio.h>
+
+#define UART_BAUD_RATE      74880
+#define UART_BAUD_PRESCALER (((F_CPU / (UART_BAUD_RATE * 16UL))) - 1)
+
 volatile int hi_or_low = 0;
 volatile int count = 0;
 
@@ -46,7 +39,7 @@ void Initialize() {
     ADMUX &= ~(1 << MUX2);
     ADMUX &= ~(1 << MUX3);
 
-    ADCSRA |= (1 << ADATE);   // Autotriggering of ADC
+    ADCSRA |= (1 << ADATE); // Autotriggering of ADC
 
     // Free running mode ADTS[2:0] = 000
     ADCSRB &= ~(1 << ADTS0);
@@ -67,24 +60,33 @@ int main(void) {
     Initialize();
     UART_init(UART_BAUD_PRESCALER);
     while (1) {
-        char intStringBuffer[10];         // Buffer to hold the converted number
-        itoa(ADC, intStringBuffer, 10);   // Convert integer to string
-        UART_putstring("ADC Read:\t");
+        char intStringBuffer[20]; // Buffer to hold the converted number
+        sprintf(intStringBuffer, "ADC Read:\t %d", ADC); // Convert integer to string
+        __PRINT_NEW_LINE__ // Make space between prints
         UART_putstring(intStringBuffer);
-        __PRINT_NEW_LINE__ __PRINT_NEW_LINE__   // Make space between prints
-            _delay_ms(5000);
+        _delay_ms(2000);
     }
 }
 #endif
 
+
+
+
 #ifdef TEMPERATURE_READ_EXAMPLE
+// Description:
+#define F_CPU               16000000UL
+#include <xc.h>
+#include "../common_libraries/uart.h"
+#include <util/delay.h>
+#include <stdio.h>
+
+#define UART_BAUD_RATE      74880
+#define UART_BAUD_PRESCALER (((F_CPU / (UART_BAUD_RATE * 16UL))) - 1)
+
 volatile int hi_or_low = 0;
 volatile int count = 0;
 
 void Initialize() {
-
-    cli();   // Disable interrupts
-
     // Setup for ADC (10bit = 0-1023)
     // Clear power reduction bit for ADC
     PRR0 &= ~(1 << PRADC);
@@ -105,7 +107,7 @@ void Initialize() {
     ADMUX &= ~(1 << MUX2);
     ADMUX |= (1 << MUX3);
 
-    ADCSRA |= (1 << ADATE);   // Autotriggering of ADC
+    ADCSRA |= (1 << ADATE); // Autotriggering of ADC
 
     // Free running mode ADTS[2:0] = 000
     ADCSRB &= ~(1 << ADTS0);
@@ -126,30 +128,40 @@ int main(void) {
     Initialize();
     UART_init(UART_BAUD_PRESCALER);
     while (1) {
-
         int currentAdcRead = ADC;
         // Datasheet 28.8: Temperature conversion
         // T = { [(ADCH << 8) | ADCL] - TOS} / k
-        char rawAdcStringBuffer[10];                    // Buffer to hold the converted number
-        itoa(currentAdcRead, rawAdcStringBuffer, 10);   // Convert integer to string
-        UART_putstring("Raw ADC Read:\t");
-        UART_putstring(rawAdcStringBuffer);
-        // UART_putstring("IC Temperature:\t");
-        // UART_putstring(intStringBuffer);
-        __PRINT_NEW_LINE__ __PRINT_NEW_LINE__   // Make space between prints
-            _delay_ms(5000);
+
+        char intStringBuffer[20]; // Buffer to hold the converted number
+        sprintf(intStringBuffer, "Raw ADC Read:\t %d", currentAdcRead); // Convert integer to string
+        __PRINT_NEW_LINE__ // Make space between prints
+        UART_putstring(intStringBuffer);
+        _delay_ms(2000);
     }
 }
 #endif
 
+
+
+
 #ifdef SIGNAL_FREQ_MEASURE_EXAMPLE
+// Description:
+#define F_CPU               16000000UL
+#include <xc.h>
+#include "../common_libraries/uart.h"
+#include <util/delay.h>
+#include <avr/interrupt.h>
+#include <stdio.h>
+
+#define UART_BAUD_RATE      74880
+#define UART_BAUD_PRESCALER (((F_CPU / (UART_BAUD_RATE * 16UL))) - 1)
 
 volatile int hi_or_low = 0;
 volatile int count = 0;
 volatile int print_flag = 0;
 
 void Initialize() {
-    cli();   // Disable global interrupts
+    cli(); // Disable global interrupts
 
     // Setup for ADC (10bit = 0-1023)
     // Clear power reduction bit for ADC
@@ -171,16 +183,16 @@ void Initialize() {
     ADMUX &= ~(1 << MUX2);
     ADMUX &= ~(1 << MUX3);
 
-    ADCSRA |= (1 << ADATE);   // Autotriggering of ADC
+    ADCSRA |= (1 << ADATE); // Autotriggering of ADC
 
     // Free running mode ADTS[2:0] = 000
     ADCSRB &= ~(1 << ADTS0);
     ADCSRB &= ~(1 << ADTS1);
     ADCSRB &= ~(1 << ADTS2);
 
-    DIDR0 |= (1 << ADC0D);   // Disable digital input buffer on ADC pin
-    ADCSRA |= (1 << ADEN);   // Enable ADC
-    ADCSRA |= (1 << ADSC);   // Start conversion
+    DIDR0 |= (1 << ADC0D); // Disable digital input buffer on ADC pin
+    ADCSRA |= (1 << ADEN); // Enable ADC
+    ADCSRA |= (1 << ADSC); // Start conversion
 
     // Timer 1 Setup
     // Timer 1 Clock divided by 256
@@ -193,9 +205,9 @@ void Initialize() {
     TIMSK1 |= (1 << OCIE1A);
     // Clear the interrupt flag
     TIFR1 |= (1 << OCF1A);
-    OCR1A = 62500;   // 1 Second
+    OCR1A = 62500; // 1 Second
 
-    sei();   // Enable global interrupts
+    sei(); // Enable global interrupts
 }
 
 ISR(ADC_vect) {
@@ -209,19 +221,20 @@ ISR(ADC_vect) {
     }
 }
 
-ISR(TIMER1_COMPA_vect) { print_flag = 1; }
+ISR(TIMER1_COMPA_vect) {
+    print_flag = 1;
+}
 
 int main(void) {
     Initialize();
     UART_init(UART_BAUD_PRESCALER);
     while (1) {
         if (print_flag) {
-            print_flag = 0;                     // Clear the print flag
-            char intStringBuffer[10];           // Buffer to hold the converted number
-            itoa(count, intStringBuffer, 10);   // Convert integer to string
-            UART_putstring("Count:\t");
+            print_flag = 0; // Clear the print flag
+            char intStringBuffer[20]; // Buffer to hold the converted number
+            sprintf(intStringBuffer, "Count:\t %d", count); // Convert integer to string
+            __PRINT_NEW_LINE__ // Make space between prints
             UART_putstring(intStringBuffer);
-            __PRINT_NEW_LINE__   // Make space between prints
         }
     }
 }
--- common_libraries/uart.h
+++ common_libraries/uart.h
@@ -1,6 +1,8 @@
 #ifndef UART_H
 #define UART_H
 
+#define __PRINT_NEW_LINE__  UART_putstring("\r\n");
+
 void UART_init(int usart_baud_rate);
 
 void UART_send(unsigned char data);
