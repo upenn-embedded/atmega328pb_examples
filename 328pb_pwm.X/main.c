cd2fa0ddfc7d67cab4831e92e998e6b315921a91
Nick McGill-Gardner <nmcgill@upenn.edu>
2025-02-16 22:07:09 -0500

    cleaned up pwm and sine wave examples

--- 328pb_pwm.X/main.c
+++ 328pb_pwm.X/main.c
@@ -1,17 +1,13 @@
 // -------------------------------------
 // Uncomment only example at a time!
-#define CTC_PWM_EXAMPLE
-// #define FAST_PWM_EXAMPLE
-//#define PHASE_CORRECT_VS_FAST_EXAMPLE
+// #define CTC_PWM_EXAMPLE
+//#define FAST_PWM_EXAMPLE
+#define PHASE_CORRECT_VS_FAST_EXAMPLE
 // -------------------------------------
 
 #ifdef CTC_PWM_EXAMPLE
-//Description: Generate a 400Hz PWM signal with a 30% duty cycle
-// using CTC mode with interrupts
-
-#include <xc.h>
-#include <avr/interrupt.h>
-
+// Description: Generate a 400Hz PWM signal with a 30% duty cycle
+// using CTC mode with interrupts on Timer 1
 // Calculation: 440Hz @ 30% duty cycle
 // Period = 1/440Hz = ~2ms
 // High time = 30% * 2ms = 0.6ms
@@ -21,6 +17,10 @@
 // Adjusting for 30% duty cycle: (36360 * 0.3)-1 = 10907
 // Adjusting for 70% duty cycle: (36360 * 0.7)-1 = 25451
 // Subtracting 1 because the count starts at 0
+
+#include <xc.h>
+#include <avr/interrupt.h>
+
 int high_time = 10907;
 int low_time = 25451;
 #define LOGIC_HIGH 1
@@ -34,13 +34,8 @@ void Initialize() {
     DDRB |= (1 << DDB1); // Set PB1 to be an output pin
     PORTB |= (1 << PORTB1); // Start with output on
 
-    // Timer1, prescale of 1
-    TCCR1B |= (1 << CS10);
-
-    // Timer1, CTC mode
-    TCCR1B |= (1 << WGM12);
-
-    
+    TCCR1B |= (1 << CS10); // Timer1, prescale of 1
+    TCCR1B |= (1 << WGM12); // Timer1, CTC mode
     TCCR1A |= (1 << COM1A0); // Toggle on compare match
     OCR1A = low_time;
     nextSignalLevel = LOGIC_LOW; // Starting with low signal
@@ -69,6 +64,11 @@ int main(void) {
 
 
 #ifdef FAST_PWM_EXAMPLE
+// Description: Generate a 400kHz PWM signal with a 75% duty cycle
+// using Fast PWM (no interrupts!) on Timer 0
+// Calculation: 440kHz @ 75% duty cycle
+// TOP = OCR0A = (16MHz/(1*440kHz))-1 = 39
+
 #include <xc.h>
 
 #define F_CPU 16000000UL
@@ -76,8 +76,7 @@ int main(void) {
 void Initialize() {
     DDRD |= (1 << DDD5); // Set PD5 as OC0B
 
-    // Timer0, prescale of 1
-    TCCR0B |= (1 << CS00);
+    TCCR0B |= (1 << CS00); // Timer0, prescale of 1
 
     // Timer0, Fast PWM mode
     TCCR0A |= (1 << WGM00);
@@ -87,56 +86,60 @@ void Initialize() {
     OCR0A = 39; // Sets frequency, 400kHz
     OCR0B = OCR0A * 1 / 4; // Sets duty cycle
 
-    // Non-inverting mode
-    // Clear on Compare Match
+    // Non-inverting mode, Clear on Compare Match
     TCCR0A |= (1 << COM0B1);
 }
 
 int main(void) {
     Initialize();
-    while (1)
-        ;
+    while (1);
 }
 #endif
 
+
 #ifdef PHASE_CORRECT_VS_FAST_EXAMPLE
 #include <xc.h>
-#include <avr/interrupt.h>
-
 #define F_CPU 16000000UL
 
 void Initialize() {
-
-    cli(); // Disable global interrupts
-
+    // === Set up Fast PWM mode (Mode 3) on Timer 0
     // Set PD5 (OC0B) and PD6 (OC0A) as outputs
     DDRD |= (1 << DDD5) | (1 << DDD6);
 
-    // Timer0, prescale of 1
+    // Timer0, prescale of 64
     TCCR0B |= (1 << CS01) | (1 << CS00);
 
-    //    // Timer0, Fast PWM mode (Mode 3)
-    //    TCCR0A |= (1 << WGM00) | (1 << WGM01);
-    //    TCCR0B |= (0 << WGM02);
+    // Timer0, Fast PWM mode (Mode 3)
+    TCCR0A |= (1 << WGM00) | (1 << WGM01);
+    TCCR0B &= ~(1 << WGM02);
 
-    // Timer0, Phase Correct PWM mode (Mode 1)
-    TCCR0A |= (1 << WGM00) | (0 << WGM01);
-    TCCR0B |= (0 << WGM02);
-
-    // Non-inverting mode
-    // Clear on Compare Match
+    // Clear on Compare Match, Non-inverting mode
     TCCR0A |= (1 << COM0A1) | (1 << COM0B1);
     TCCR0A &= ~((1 << COM0A0) | (1 << COM0B0));
 
     OCR0A = 200; // Sets frequency
     OCR0B = OCR0A * 1 / 4; // Sets duty cycle, 75%
 
-    sei(); // Enable global interrupts
+    // === Set up Phase Correct PWM mode (Mode 1) on Timer 1
+    // Set PB1 (OC1A) and PB2 (OC1B) as outputs
+    DDRB |= (1 << DDB1) | (1 << DDB2);
+
+    // Timer1, prescale of 64
+    TCCR1B |= (1 << CS11) | (1 << CS10);
+
+    // Timer1, Phase Correct PWM mode, 8-bit (Mode 1)
+    TCCR1A |= (1 << WGM10);
+
+    // Clear on Compare Match, Non-inverting mode
+    TCCR1A |= (1 << COM1A1) | (1 << COM1B1);
+    TCCR1A &= ~((1 << COM1A0) | (1 << COM1B0));
+
+    OCR1A = 200; // Sets frequency
+    OCR1B = OCR1A * 1 / 4; // Sets duty cycle, 75%
 }
 
 int main(void) {
     Initialize();
-    while (1)
-        ;
+    while (1);
 }
 #endif
--- 328pb_sineWave.X/main.c
+++ 328pb_sineWave.X/main.c
@@ -1,28 +1,24 @@
+// Description: Approximate a sine wave using a lookup table and duty cycle variation.
 #include <xc.h>
 #include <avr/interrupt.h>
-#include <stdlib.h>
 
 #define F_CPU 16000000UL
-
 int size = 100;
+volatile int lookup_count = 0;
 
 const int sine_table_const[100] = {128, 136, 143, 151, 159, 167, 174, 182, 189, 196, 202, 209, 215, 220, 226, 231, 235, 239, 243, 246, 249, 251, 253, 254, 255,
     255, 255, 254, 253, 251, 249, 246, 243, 239, 235, 231, 226, 220, 215, 209, 202, 196, 189, 182, 174, 167, 159, 151, 143, 136,
     128, 119, 112, 104, 96, 88, 81, 73, 66, 59, 53, 46, 40, 35, 29, 24, 20, 16, 12, 9, 6, 4, 2, 1, 0,
     0, 0, 1, 2, 4, 6, 9, 12, 16, 20, 24, 29, 35, 40, 46, 53, 59, 66, 73, 81, 88, 96, 104, 112, 119};
 
-volatile int lookup_count = 0;
-
 void Initialize(void) {
-    // Disable global interrupts
-    cli();
+    cli(); // Disable global interrupts
 
     // GPIO pin setup
     DDRB |= (1 << DDB1); // OC1A - PB1
     DDRB |= (1 << DDB5); // Debug output pin
 
-    // Timer 1 Setup
-    // Prescale of 1
+    // Timer 1 Setup, Prescale of 1
     TCCR1B |= (1 << CS10);
 
     // Set Phase Correct PWM
@@ -40,8 +36,7 @@ void Initialize(void) {
     // Clear on compare match
     TCCR1A |= (1 << COM1A1);
 
-    // Enable global interrupts
-    sei();
+    sei(); // Enable global interrupts
 }
 
 ISR(TIMER1_OVF_vect) {
@@ -57,6 +52,5 @@ ISR(TIMER1_OVF_vect) {
 
 int main(void) {
     Initialize();
-    while (1)
-        ;
+    while (1);
 }
